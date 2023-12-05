5ddfa320a97122fd41ff63441546e207d05236e7
Oumadenis1779 <dennies.ouma@student.moringaschool.com>
2023-12-05 08:43:56 +0300

    complete assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,41 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        self._name= None
+        self._breed= None
+
+        if name is not None:
+           self.name=name
+        if breed is not None:
+           self.breed= breed
+
+
+    def name (self):
+       return self._name
+    
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.\n")
+            
+  
+    def breed(self):
+        return self._breed
+        
+
+    
+    def breed(self, value):
+        if value in self.approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
+
+       
+
+fido= Dog("Mastiff")
+
+
+
+    
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,44 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+
+    def __init__(self, name, job):
+        self._name = None
+        self._job = None
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) < 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in self.approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in the list of approved jobs.")
+
+# Example usage:
+#person1 = Person("john doe", "Engineer")
+#print(person1.name)  # Output: John Doe
+#print(person1.job)   # Output: Engineer
+
+#person2 = Person("Jane Smith", "Artist")
+#print(person2.name)  # Output: Jane Smith
+#print(person2.job)   # Output: Artist
+
+#person3 = Person("Bob Johnson", "Pilot")  # Output: Job must be in the list of approved jobs.
+#person3.name = "ThisIsAVeryLongNameForAPerson"  # Output: Name must be a string under 25 characters.
+
